070d722bf9225cab9e485bbf273f92f2ace8c661
suhailsaeedy-design <suhailsaeedy@gmail.com>
2026-09-23 09:57:53 +0430

    Use Phase 21 offline pack cache prefix

--- assets/js/offline-packs.js
+++ assets/js/offline-packs.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const MANIFEST_URL = './data/offline-packs.json';
-  const prefix='smd-v21-phase19-pack-';
+  const prefix='smd-v21-phase21-pack-';
 
   const $ = (selector) => document.querySelector(selector);
   const $$ = (selector) => [...document.querySelectorAll(selector)];
