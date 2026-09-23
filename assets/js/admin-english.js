001f0b108b19335167497535c6ddc52c28d1dc6c
suhailsaeedy-design <suhailsaeedy@gmail.com>
2026-09-23 21:02:51 +0430

    Force private admin pages to English without changing public language preference

---
+++ assets/js/admin-english.js
@@ -0,0 +1,8 @@
+(() => {
+  'use strict';
+  window.SMD21=window.SMD21||{};
+  window.SMD21.state=window.SMD21.state||{};
+  window.SMD21.state.lang='en';
+  document.documentElement.lang='en';
+  document.documentElement.dir='ltr';
+})();
